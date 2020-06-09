bac751af26eddc59b00c97b4133469c2b349e392
Lucas Kolinger <lucaskolinger@icloud.com>
2020-06-09 14:46:09 +0200

    initial commit

---
+++ pages/inspire.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+        <div><h1>DIES IST EIN TEST FÜR EINE GIT REMOTE REPO</h1></div>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
